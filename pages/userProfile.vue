cb675db74b0cb1008b2e555cedd36843365e5634
hammas-19 <hammasmasood@gmail.com>
2023-01-17 22:23:30 +0500

    style: designed dashboard sections

--- pages/userProfile.vue
+++ pages/userProfile.vue
@@ -1,13 +1,12 @@
       
 <script>
-import Icons from '~~/assets/icons/icons.vue';
 export default {
     data() {
         return {
-            activeTab: 'tab1',
+            activeTab: 'Dashboard',
             showList: window.innerWidth > 768,
-            tabs: ['tab1', 'tab2', 'tab3']
-
+            tabs: ['Dashboard', 'tab1', 'tab2', 'tab3'],
+            showProfile: true,
         }
     }
 }
@@ -23,9 +22,12 @@ definePageMeta({
     height: -moz-calc(100vh - 60px);
     height: calc(100vh - 60px);
 }
-
+.content{
+    direction: rtl;
+}
 .content::-webkit-scrollbar-track {
-    background-color: #88e2a4;
+    background-color: #b6b4b4;
+
 }
 
 .content::-webkit-scrollbar {
@@ -34,13 +36,11 @@ definePageMeta({
 }
 
 .content::-webkit-scrollbar-thumb {
-    background-color: #6a8ddf;
+    background-color: #b6b4b4;
     background-image: -webkit-linear-gradient(90deg,
-            rgba(224, 28, 28, 0.964) 0%,
-            #88a3e2 25%,
-            transparent 100%,
-            rgb(132, 23, 209) 75%,
-            transparent)
+            #6a6969 0%,
+            #878686 25%,
+            transparent 100%)
 }
 </style>
 <template>
@@ -51,30 +51,104 @@ definePageMeta({
                 class="h-10 w-20 fixed top-0   ">
                 Show/Hide
             </button>
+            <button @click="showProfile = !showProfile" :class="{ 'bg-blue-500': showList, 'bg-gray-500': !showList }"
+                class="h-10 w-20 fixed top-0 right-0">
+                Show/Hide
+            </button>
 
             <div
-                class="main flex container mx-auto my-10 relative rounded-[20px] md:rounded-[30px] border-[3px] border-nectarine border-dashed overflow-hidden">
+                class="main bg-[#d0cece] flex container mx-auto my-10 relative rounded-[20px] md:rounded-[30px] border-[3px] border-astronaut border-dashed overflow-hidden">
 
 
 
                 <!-- Tabs vertical -->
-                <div class="tabs text-white bg-astronaut border-2 border-red-400 relative">
-
+                <div class="tabs text-astronaut bg-mercury relative rounded-l-[30px] ">
+                    <div class="border-r-[3px] border-[#b6b4b4] border-dashed py-5 h-full">
                     <!-- Notification Icon  -->
+                    <span class="top-1 right-2 absolute cursor-pointer ">
+                        <img src="images/index_v2/icons/notification.png"
+                            class="h-6 w-6 hover:h-8 hover:w-8 transition-all" alt="notifictaions">
+                    </span>
+
+                    <ul v-if="showList" class="nav-tabs text-left md:gap-8 gap-3 flex flex-col md:min-w-[240px] min-w-[140px]">
+
+                        <!-- Dp & Name -->
+                        <div class="flex justify-between mx-auto md:py-4 py-1 flex-col items-center md:gap-5 gap-3">
+                            <span
+                                class="rounded-full md:h-24 h-20 md:w-24 w-20 border-[3px] border-astronaut border-dashed text-center flex justify-center items-center">
+                                <img src="images/index_v2/icons/user-profile.png" class="h-8 w-8" alt="displayPicture">
+                            </span>
+                            <span class="text-astronaut md:text-2xl text-lg font-semibold text-center">
+                                Hammas Masood
+                            </span>
+
+                        </div>
+                        <div class="">
+                            <li v-for="(tab, index) in tabs" :key="index"
+                                class="mx-10 md:my-5 my-1 px-5 text-center md:py-3 py-1 rounded-xl cursor-pointer"
+                                :class="{ 'border-[3px] border-[#FF8656] border-dashed text-[#FF8656] font-semibold': activeTab === tab }"
+                                @click="activeTab = tab">
+                                {{ tab }}
+                            </li>
+                        </div>
+
+                        <!-- Journey button -->
+                        <div class="absolute bottom-0 flex flex-col m-[2px] border-4 border-red-500">
+                            <div @click="showProfile = !showProfile"
+                                class="md:text-lg text-base text-center font-semibold py-3 px-5 flex justify-center items-center border-[3px] border-[#b6b4b4] border-dashed m-2 rounded-xl  hover:text-mercury hover:bg-astronaut transition-all">
+                                Resume Journey
+                            </div>
+                            <span class="md:h-28 h-10 w-[1px] border-l-[3px] border-[#b6b4b4] border-dashed mx-auto">
+                                <!-- just line -->
+                            </span>
+                            <img src="images/index_v2/walkingman.gif" class="rounded-[30px] max-h-[260px]" alt="manwalking">
+                        </div>
+
+                    </ul>
+                    </div>
+                </div>
+
+                <!-- Contents -->
+                <div
+                    class=" bg-mercury md:p-5 p-1 rounded-[30px] rounded-l-none w-full">
+                    
+                    <div class="content h-full overflow-auto">
+
+                    <div v-show="activeTab === 'tab1'" class="flex flex-wrap justify-center">
+                        <ShowCard />
+                    </div>
+                    <div v-show="activeTab === 'tab2'">
+                        <Govtob />
+                    </div>
+                    <div v-show="activeTab === 'tab3'">
+                        <stats />
+                    </div>
+                    </div>
+
+                </div>
+
+
+                <!-- Profile Tab -->
+                <div class="tabs text-astronaut bg-[#d0cece] relative rounded-r-[30px]">
+                    
+                    <div  v-if="showProfile" class="md:min-w-[340px] min-w-[240px] p-4">
+
+                                            <!-- Notification Icon  -->
                     <span class="top-1 right-1 absolute cursor-pointer ">
                         <img src="images/index_v2/icons/notification.png"
                             class="h-6 w-6 hover:h-8 hover:w-8 transition-all" alt="notifictaions">
                     </span>
 
-                    <ul v-if="showList" class="nav-tabs text-left gap-8 flex flex-col md:min-w-[240px] min-w-[140px] ">
+                    <ul
+                        class="nav-tabs text-left gap-8 flex flex-col ">
 
                         <!-- Dp & Name -->
                         <div class="flex justify-between mx-auto py-4 flex-col items-center gap-5">
                             <span
-                                class="rounded-full h-24 w-24 border-[3px] border-nectarine border-dashed text-center flex justify-center items-center">
+                                class="rounded-full h-24 w-24 border-[3px] border-astronaut border-dashed text-center flex justify-center items-center">
                                 <img src="images/index_v2/icons/user-profile.png" class="h-8 w-8" alt="displayPicture">
                             </span>
-                            <span class="text-white text-2xl font-semibold">
+                            <span class="text-astronaut md:text-2xl text-xl font-semibold text-center">
                                 Hammas Masood
                             </span>
 
@@ -88,25 +162,23 @@ definePageMeta({
                             </li>
                         </div>
 
+
+                        <div class="absolute bottom-0 flex flex-col m-[2px]">
+                            <div
+                                class="md:text-lg text-base text-center font-semibold py-3 px-5 flex justify-center items-center border-[3px] border-[#b6b4b4] border-dashed m-2 rounded-xl  hover:text-mercury hover:bg-astronaut transition-all">
+                                Resume Journey
+                            </div>
+                            <span class="h-28 w-[1px] border-l-[3px] border-[#b6b4b4] border-dashed mx-auto">
+                                <!-- just line -->
+                            </span>
+                            <img src="images/index_v2/walkingman.gif" class="rounded-[30px] " alt="manwalking">
+                        </div>
+
                     </ul>
 
-                    <div class="absolute bottom-1">
-                        <img src="images/index_v2/walkingman.gif" class="w-[95%] mx-auto rounded-[30px]" alt="manwalking">
                     </div>
 
-                </div>
 
-                <!-- Contents -->
-                <div class=" bg-myst border-2 border-blue-400 overflow-y-scroll content">
-                    <div v-show="activeTab === 'tab1'" class="flex flex-wrap justify-center">
-                        <ShowCard />
-                    </div>
-                    <div v-show="activeTab === 'tab2'">
-                        <Govtob />
-                    </div>
-                    <div v-show="activeTab === 'tab3'">
-                        <stats />
-                    </div>
                 </div>
 
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
 				greatVibes: ['Great Vibes', 'cursive']
 			},
 			colors: {
+				mercury:"#e6e6e6",
 				myst:"#e7ecef",
 				astronaut:"#274c77",
         			nectarine: "#FF8656",
