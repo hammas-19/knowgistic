5d1e1fbab8a7ab3e5e9ffb7ed32bf22b79c55c44
hammas-19 <hammasmasood@gmail.com>
2023-01-26 22:35:20 +0500

    style: designed jobs course card

---
+++ components/jobCourseCard.vue
@@ -0,0 +1,36 @@
+<style scoped>
+.Info::-webkit-scrollbar {
+    display: none;
+}
+</style>
+<template>
+    <div class="md:w-[580px] w-[300px] md:h-[463px] h-[343px] flex gap-2 rounded-3xl p-2 bg-white shadow-2xl">
+
+
+        <!-- Info -->
+        <div class="Info flex flex-col p-4 rounded-3xl bg-[#f3f1fc] w-1/2 overflow-y-scroll">
+
+            <div v-for="items in 12" class="flex items-start gap-2">
+                <span><img src="/images/Gallery/Ellipse.svg" class="w-12 mt-1" alt="elipse"></span>
+                <div class="flex flex-col gap-2">
+                    <span class="text-lg text-ebonyClay font-medium">Lorem ipsum dolor sit amet consectetur.</span>
+                    <span class="text-xs text-comet">Lorem ipsum dolor sit amet consectetur. Gravida quam faucibus ornare egestas mi in mattis
+                        auctor.</span>
+                </div>
+            </div>
+
+        </div>
+  
+        <!-- ImageGraphy -->
+        <div class="h-full w-1/2 rounded-3xl bg-midHeart relative text-4xl text-white">
+
+            <div class="absolute w-full text-center bottom-5">
+            <h1 class="text-4xl font-bold font-jost">FA/FSC</h1>
+            <span class="text-sm font-jost ">12 courses . 18quizzes</span>
+            </div>
+
+        </div>
+
+
+    </div>
+</template>
--- pages/home.vue
+++ pages/home.vue
@@ -1,11 +1,33 @@
 <template>
         <div>
+            
+            <!-- Hero -->
             <section class="bg-selago relative">
                 <div class="flex justify-center">
                         <HeroNew />
                 </div>
+            </section>
+
+            <!-- Gallery -->
+            <section class="bg-selago">
+
+                <!-- Typo/Info -->
+                <div class="py-14 flex flex-col max-w-[402px] gap-3 text-center justify-center mx-auto">
+
+                    <span class="text-purplHeart font-medium">Lorem ipsum dolor sit ipsum dolor</span>
+                    <span class="text-4xl font-bold text-ebonyClay">Lorem ipsum dolor sit ipsum dolor</span>
+                    <span class="text-comet text-xs">Lorem ipsum dolor sit amet consectetur. A lectus volutpat diam quis.</span>
+                
+                </div>  
+
+                <!-- Open Cards -->
+                <div class="flex justify-evenly">
+                    <JobCourseCard v-for="items in 2" :class="{'flex-row-reverse' : items === 2}" />
+                </div>
 
 
             </section>
+
+
         </div>
 </template>
