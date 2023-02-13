b65ee2989f674bea37f797b30a34af0d34b5afed
hammas-19 <hammasmasood@gmail.com>
2023-02-13 22:49:08 +0500

    style: Designed filters for exam category

---
+++ components/jobElement.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex w-full md:max-w-md max-w-[120px] mx-auto overflow-hidden bg-white rounded-lg shadow-lg animate-pulse dark:bg-gray-800">
+    <div class="w-1/3 bg-purplHeart"></div>
+
+    <div class="w-2/3 p-4 md:p-4">
+        <h1 class="md:w-40 w-10 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+
+        <p class="md:w-48 w-16 h-2 mt-4 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+
+        <div class="flex mt-4 item-center gap-x-2">
+            <p class="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p class="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p class="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p class="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p class="w-5 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+        </div>
+
+        <div class="flex justify-between mt-6 item-center">
+            <h1 class="md:w-10 w-4 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></h1>
+
+            <div class="h-4 bg-gray-200 rounded-lg w-28 dark:bg-gray-700"></div>
+        </div>
+    </div>
+</div>
+</template>
