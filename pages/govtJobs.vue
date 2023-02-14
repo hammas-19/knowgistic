2da906850d922fe83c64d0118cc3b450d0d7092c
Hammas-19 <Hammasmasood@gmail.com>
2023-02-15 00:14:46 +0500

    style: Designed jobs page

---
+++ pages/govtJobs.vue
@@ -0,0 +1,28 @@
+<template>
+        <div>
+
+                <section>
+
+                        <PageBanner>
+                                <template #bannerImg>
+                                        <img src="/images/index_v2/Quizbg.png"
+                                                class="h-full w-full absolute opacity-20 z-0 object-none" alt="">
+                                </template>
+
+                                <template #head>Job </template>
+
+                                <template #foot> Exams </template>
+
+                        </PageBanner>
+
+                </section>
+
+                <!-- Gallery -->
+                <section>
+                        <div class="lg: container mx-auto px-9 flex flex-col md:flex-row flex-wrap justify-center gap-8">
+                                <JobCourseCard v-for="items in 8" />
+                        </div>
+                </section>
+
+        </div>
+</template>
