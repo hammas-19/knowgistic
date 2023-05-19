97ebeb54f4b478737d39dca45bab280503abb5cd
hammas-19 <hammasmasood@gmail.com>
2023-05-19 18:38:46 +0500

    style: minor changes

--- components/user/avatar.vue
+++ components/user/avatar.vue
@@ -1,7 +1,7 @@
 <template>
     
 <div class="relative inline-flex items-center justify-center max-w-40 max-h-40 overflow-hidden bg-purplHeart rounded-full ">
-    <span class="font-medium text-white">JL</span>
+    <img src="https://demos.creative-tim.com/soft-ui-dashboard-tailwind/assets/img/bruce-mars.jpg" alt="">
 </div>
 
 </template>
--- pages/userDashboard/[slug].vue
+++ pages/userDashboard/[slug].vue
@@ -1,7 +1,8 @@
 <style scoped>
-*{
+* {
     scroll-behavior: smooth;
 }
+
 .window::-webkit-scrollbar-track {
     background-color: #f0ecfd;
 
@@ -19,14 +20,15 @@
             #e4daff 25%,
             transparent 100%)
 }
+
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s ease;
+    transition: opacity 0.5s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
 <template>
@@ -35,12 +37,11 @@
         <!-- Header -->
         <section class="relative max-w-7xl mx-auto p-3">
 
-            <span @click="showNav = !showNav" class="cursor-pointer block md:hidden absolute right-2 top-2 text-purplHeart">
+            <span @click="showNav = !showNav" class="cursor-pointer block md:hidden absolute right-2 top-2 text-purplHeart z-30">
                 Show/Hide
             </span>
 
-            <div class="flex md:flex-row flex-col justify-between gap-3 md:items-end items-center relative"
-                :class="{ 'hidden': !showNav }">
+            <div v-if="showNav" class="flex md:flex-row flex-col justify-between gap-3 md:items-end items-center relative">
                 <NuxtLink to="/">
                     <img src="/Logo.svg" class="w-40" alt="Logo">
                 </NuxtLink>
@@ -53,8 +54,8 @@
                 </div>
 
                 <div class="flex gap-4">
-                    
-                    <UserNotification class=""/>
+
+                    <UserNotification class="" />
                     <UserAvatar class="h-10 w-10" />
                 </div>
             </div>
@@ -68,21 +69,23 @@
 
                 <!-- Side Navigation -->
                 <Transition name="fade">
-                <div v-if="show" class="bg-whiteLilac w-full max-w-xs max-h-[calc(100vh-160px)] h-full overflow-y-scroll window "
-                :class="[{ 'absolute z-20': show }, { 'hidden md:block': !show }]">
-                    
+                    <div v-if="show"
+                        class="bg-whiteLilac w-full max-w-xs max-h-[calc(100vh-160px)] h-full overflow-y-scroll window flex flex-col"
+                        :class="[{ 'absolute z-20': show }, { 'hidden md:block': !show }]">
 
-                    <div class="flex flex-col gap-5 py-10 px-3 ">
 
-                        <UserAvatar class="self-center h-40 w-40 bg-whiteLilac" />
-                        <UserProfileBadge v-for="items in 2" />
+                        <div class="flex flex-col gap-5 py-10 px-3  bg-contain bg-[url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSQCmb-mshqNXT_1kGtaSdoVMkgzLEgIA5yETZQc1I_wh6-KT5fS3NNsERfgtPPtnMT_nLTk0q2_mVjN84gIUE6OzmnTW-F08e78AzT_-tdxl7C52HfDO5xPtmsmtZ-0pSS4D36QumOBI3-aMNsGPi85uMDn6YRxKNFoc8OFAags7ihZlskFuuYC62/s320/profileBg.jpg')]">
+
+                            <UserAvatar class="self-center h-40 w-40 bg-whiteLilac" />
+                        </div>
+                        <!-- <UserProfileBadge v-for="items in 2" /> -->
+
                     </div>
-
-                </div>
-            </Transition>
+                </Transition>
 
                 <!-- content -->
-                <div class="flex flex-wrap gap-10  md:max-h-[calc(100vh-160px)] max-h-[calc(100vh-80px)] overflow-y-scroll window py-3 w-full" :class="{'opacity-40 transition-all duration-500':show}">
+                <div class="flex flex-wrap gap-10  md:max-h-[calc(100vh-160px)] max-h-[calc(100vh-80px)] overflow-y-scroll window py-3 w-full"
+                    :class="{ 'opacity-40 transition-all duration-500': show }">
 
                     <!-- Quick OverView -->
                     <div id="overview" class="p-2 w-full transition-all duration-500">
@@ -115,21 +118,21 @@
 
                             </div>
                             <div class="p-2 shadow-inner rounded-2xl bg-whiteLilac h-fit py-5">
-                                <UserProgressBar class="w-full  mx-auto"/>
+                                <UserProgressBar class="w-full  mx-auto" />
                             </div>
 
                         </div>
 
                     </div>
                     <div id="report" class="flex gap-3 flex-wrap w-full justify-between p-2">
-                        <CourseCard v-for="items in 8" />
+                        <!-- <CourseCard v-for="items in 8" /> -->
                     </div>
                     <Transition name="fade">
 
-                    <div id="updates" class="flex gap-3 flex-wrap w-full justify-between p-2">
-                        <UserProfileCourses v-for="items in 8" />
-                    </div>
-                </Transition>
+                        <div id="updates" class="flex gap-3 flex-wrap w-full justify-between p-2">
+                            <UserProfileCourses v-for="items in 8" />
+                        </div>
+                    </Transition>
                 </div>
 
             </div>
@@ -143,5 +146,5 @@ definePageMeta({
     layout: "",
 });
 const show = ref(true)
-const showNav = ref(true)
+const showNav = ref(false)
 </script>
