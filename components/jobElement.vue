415c54832d89731377e5a4cd30820ac61caf813c
hammas-19 <hammasmasood@gmail.com>
2023-07-22 17:24:23 +0500

    style: changed job Cards in category section

--- components/jobElement.vue
+++ components/jobElement.vue
@@ -1,39 +1,40 @@
 <template>
+    <div
+        class="flex gap-2 items-center border border-whiteLilac md:max-w-[280px] max-w-[140px] w-full md:p-2 p-1 rounded-xl bg-white transition-all hover:shadow-lg shadow-whiteLilac cursor-pointer">
 
-    <div class="flex flex-col md:flex-row gap-2 items-center border border-whiteLilac max-w-[280px] w-full md:p-2 p-1 rounded-xl bg-white transition-all hover:shadow-lg shadow-whiteLilac cursor-pointer">
-    
         <span class="md:w-1/3 w-2/4">
             <img src="/images/GOVTpk.png" alt="">
         </span>
-        <div class="text-comet text-sm p-2 rounded-lg flex flex-col justify-around gap-1 bg-whiteLilac w-full items-center text-center md:items-start md:text-left md:h-28">
+        <div
+            class="text-comet text-xs md:p-2 p-1 md:rounded-lg rounded-md flex flex-col justify-around md:gap-1 gap-[2px] bg-whiteLilac w-full md:h-28 h-20 md:text-sm">
             <span class="font-semibold">
                 {{ props.title }}
             </span>
-            <span class="text-sm font-semibold">
-                <span class="font-normal">No. of posts :</span> {{ props.posts }} 
-    
+            <span class="font-semibold">
+                <span class="font-normal">No. of posts :</span> {{ props.posts }}
+
             </span>
             <span class="text-xs">
-               Test Date : {{ props.dated }}
-    
+                Test Date : {{ props.dated }}
+
             </span>
         </div>
-    
+
     </div>
-    </template>
-    <script setup>
-    const props = defineProps({
-        title: {
+</template>
+<script setup>
+const props = defineProps({
+    title: {
         type: String,
         required: true
-        },
-        posts: {
+    },
+    posts: {
         type: String,
         required: true
-        },
-        dated: {
+    },
+    dated: {
         type: String,
         required: true
-      }
-    })
-    </script>
+    }
+})
+</script>
